c82c95303b69fbae76b8f92f2b66e8d0aa7b162e
High Wind <nguyenhai@gmail.com>
2025-04-16 12:30:24 -0400

    removed weird comment on developer screen

--- src/ScrumPointingApp.jsx
+++ src/ScrumPointingApp.jsx
@@ -686,7 +686,6 @@ export default function ScrumPointingApp() {
                     )}
                     */}
 
-  // add change vote button
 {isDeveloper && !votesRevealed && (
   <>
     <div className="grid grid-cols-3 gap-4 mb-4 mt-6">
